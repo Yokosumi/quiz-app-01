0f3859f414bb84e332e29e187ddb6d1f81bb9f24
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-10-14 15:49:34 +0200

    minor style changes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import "./style.css";
 import { themeChange } from "theme-change";
 themeChange();
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /*html*/ `
- <div class="m-2 flex flex-col w-32">
-	<h2 class="mb-4 text-2xl text-blue-500 font-bold">Select</h2>
-	<select data-choose-theme class="focus:outline-none h-10 rounded-full px-3 border">
+ <div class="m-2 flex flex gap-3">
+	<h2 class="mb-4 text-2xl text-blue-500 font-bold">Themes:</h2>
+	<select data-choose-theme class="focus:outline-none h-10  px-3 border">
 		<option value="light">light</option>
 		<option value="dark">dark</option>
 		<option value="dracula">dracula</option>
